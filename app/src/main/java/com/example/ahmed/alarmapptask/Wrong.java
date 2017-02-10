52a9ab5370048a7f60ad345326054e1c274c12e7
ahmedmedhat187 <ahmed.medhat187@gmail.com>
2017-02-10 22:34:44 +0200

    Add wrong activity

--- app/src/main/java/com/example/ahmed/alarmapptask/Wrong.java
+++ app/src/main/java/com/example/ahmed/alarmapptask/Wrong.java
@@ -2,19 +2,30 @@ package com.example.ahmed.alarmapptask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Wrong extends AppCompatActivity {
 
+    Button btn_back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wrong);
+
+        btn_back = (Button)findViewById(R.id.btn_back);
 
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        finish();
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
